523df2139bb43419aed84faa43a267ec11eb3d51
Unknown <magerusan@gmx.at>
2018-07-24 20:22:25 +0200

    add change logo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  Agape Wien
+  <img class="logo" src="static/logo.png" alt="Agape">
   <toolbar>
   </toolbar>
 </div>
@@ -24,18 +24,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.logo {
+  width: 200px;
 }
 </style>
