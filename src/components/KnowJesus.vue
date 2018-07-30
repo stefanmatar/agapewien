61197cbbcfeb323dc542587eaffa2296b6ff172d
Unknown <magerusan@gmx.at>
2018-07-30 13:39:56 +0200

    update KnowJesus

--- src/components/KnowJesus.vue
+++ src/components/KnowJesus.vue
@@ -1,13 +1,51 @@
-<template>
-<h1>Know Jesus - Know Peace</h1>
-</template>
-
-<script>
-export default {
-  name: 'KnowJesus'
-}
-</script>
-
-<style>
-
+<template> 
+<section id="Know"> 
+  <div class="container-fluid"> 
+  <div class="row"> 
+      <div class="col-12"> 
+          <div class="bg bg-center"> 
+            <h2>Know Jesus - Know Peace</h2> 
+            <div class="container"> 
+            <div class="btn btn-light btn-rounded">  
+              MEHR DAZU 
+            </div> 
+            </div> 
+          </div> 
+      </div> 
+  </div> 
+</div> 
+</section> 
+</template> 
+ 
+<script> 
+ 
+export default { 
+  name: 'KnowJesus' 
+} 
+</script> 
+ 
+<style> 
+.PlayButton { 
+  width: 15px; 
+} 
+.container{ 
+  margin-bottom: 1em; 
+  margin-top: 1em; 
+} 
+ 
+.bg { 
+    color: #FFFFFF; 
+    padding: 250px; 
+ 
+} 
+ 
+.container-fluid { 
+    padding-left: 0px; 
+    padding-right: 0px; 
+} 
+.bg-center { 
+    background: url("https://media.swncdn.com/cms/BST/28275-cross-fog1200.800w.tn.jpg"); 
+    height:100%; 
+} 
+ 
 </style>
